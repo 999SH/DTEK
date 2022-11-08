6bc75dc51d27518a6039e541aa6a407cb3c5237d
999SH <94057132+999SH@users.noreply.github.com>
2022-11-08 19:16:39 +0100

    Lab 2 started

--- Lab2/prime.c
+++ Lab2/prime.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 int is_prime(int n){
+  int *i;
+  i = &n;
+  printf("%p is the adress of i\n",&i);
+  printf("%p is the adress in i\n",i);
+  printf("%p is the adress of n\n",&n);
+  printf("%d is the value of i\n",*i);
   return 0;
 }
 
 int main(void){
   printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
-  printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
-  printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
+  //printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
+  //printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
 }
