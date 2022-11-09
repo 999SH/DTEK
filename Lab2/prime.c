b72079ce67594989e9e4bdd4c3616b30a71388de
999SH <94057132+999SH@users.noreply.github.com>
2022-11-09 15:20:17 +0100

    Lab 2 task 1 done

--- Lab2/prime.c
+++ Lab2/prime.c
@@ -1,17 +1,37 @@
 #include <stdio.h>
 
 int is_prime(int n){
+  int i = 2;
+  while (i < n){
+    if (n%i == 0){
+      return 0;
+    }
+    i++;
+  }
+  return 1;
+}
+
+int test(int a){
   int *i;
-  i = &n;
+  i = &a;
+  int *p = &a;
   printf("%p is the adress of i\n",&i);
   printf("%p is the adress in i\n",i);
-  printf("%p is the adress of n\n",&n);
+  printf("%p is the adress of a\n",&a);
   printf("%d is the value of i\n",*i);
+  *i = 15;
+  printf("%p is the adress in i\n",i);
+  printf("%d is the value of a\n",a);
+  printf("%p is the adress stored in p\n",p);
+  printf("%d is the value stored in *p\n",*p);
   return 0;
 }
 
 int main(void){
+  test(240);
   printf("%d\n", is_prime(11));  // 11 is a prime.      Should print 1.
-  //printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
-  //printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
+  printf("%d\n", is_prime(4));  // 11 is a prime.      Should print 0.
+  printf("%d\n", is_prime(383)); // 383 is a prime.     Should print 1.
+  printf("%d\n", is_prime(987)); // 987 is not a prime. Should print 0.
 }
+
